28225169f53a8ea25b7e108db71421888f7f232c
lilXA1 <abedshaaban600@gmail.com>
2022-02-27 19:37:23 +0200

    signup update input value

--- src/Components/DashbordRedirect.js
+++ src/Components/DashbordRedirect.js
@@ -9,40 +9,33 @@ const DashbordRedirect = () => {
 
   const dispatch = useDispatch();
 
-  useEffect(
-    () => {
-      auth.onAuthStateChanged((userAuth) => {
-        if (userAuth) {
-          // user is  logged in
-          dispatch(
-            login({
-              email: userAuth.email,
-              uid: userAuth.uid,
-              displayName: userAuth.displayName,
-              photoURL: userAuth.photoURL,
-            })
-          );
-        } else {
-          // user is  logged out
-          dispatch(logout());
-        }
-      });
-    },
-    // i added disdpatch
-    [dispatch]
-  );
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // user is  logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        );
+      } else {
+        // user is  logged out
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
   return (
     <>
       {!user ? (
-        // <Login />
         <>
           {/* <h2>hi</h2> */}
-          <h2>no</h2>
+          <Navigate to="/signup" />
         </>
       ) : (
-        <div className="app__body">
-          <h2>hi</h2>
-          {/* <Navigate to="/questions/1" replace /> */}
+        <div>
+          <h2>logged in</h2>
         </div>
       )}
     </>
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,8 +1,15 @@
 import { Box, Button, Card, TextField } from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Styles/Signup.css";
 
 const Signup = () => {
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [repeatPassword, setrepeatPassword] = useState("");
+
   return (
     <div className="signup_div contentcenter padtop20">
       <Box
@@ -23,26 +30,36 @@ const Signup = () => {
           <h2 className="logincenter">Signup</h2>
           <div className="contentcentercolumn">
             <TextField
+              value={FirstName}
+              onChange={(e) => setFirstName(e.target.value)}
               sx={{ margin: "20px" }}
               label="First Name"
               variant="standard"
             />
             <TextField
+              value={LastName}
+              onChange={(e) => setLastName(e.target.value)}
               sx={{ margin: "20px" }}
               label="Last Name"
               variant="standard"
             />
             <TextField
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
               sx={{ margin: "20px" }}
               label="Email"
               variant="standard"
             />
             <TextField
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
               sx={{ margin: "20px" }}
               label="Password"
               variant="standard"
             />
             <TextField
+              value={repeatPassword}
+              onChange={(e) => setrepeatPassword(e.target.value)}
               sx={{ margin: "20px" }}
               label="Repeat Password"
               variant="standard"
@@ -52,7 +69,13 @@ const Signup = () => {
             <Link to="/" className="link">
               <Button variant="outlined">cancel</Button>
             </Link>
-            <Button variant="outlined">signup</Button>
+
+            <Button
+              // onClick={}
+              variant="outlined"
+            >
+              signup
+            </Button>
           </div>
         </Card>
       </Box>
